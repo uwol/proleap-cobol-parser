968cb4277e34ce170c5555c78fce6897f5db3ee7
uwol <u.wol@wwu.de>
2015-07-29 23:58:46 +0200

    preprocessor

---
+++ src/main/java/org/cobol85/preprocessor/KpirPreprocessor.java
@@ -0,0 +1,30 @@
+/*
+Copyright (C) 2015 u.wol@wwu.de
+
+This file is part of cobol85grammar.
+
+cobol85grammar is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+cobol85grammar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with cobol85grammar. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.cobol85.preprocessor;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface KpirPreprocessor {
+
+	String process(File inputFile) throws IOException;
+
+	String process(String input);
+}
