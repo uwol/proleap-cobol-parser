c916a821915e6971bee268945f3e685c9ee2c830
uwol <u.wol@wwu.de>
2016-01-30 17:56:45 +0100

    NIST test suite

---
+++ src/test/java/org/cobol85/nist/OBNC1MTest.java
@@ -0,0 +1,20 @@
+package org.cobol85.nist;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.cobol85.applicationcontext.Cobol85GrammarContextFactory;
+import org.cobol85.runner.Cobol85ParseTestRunner;
+import org.cobol85.runner.impl.Cobol85ParseTestRunnerImpl;
+
+public class OBNC1MTest {
+
+	@Test
+	public void test() throws Exception {
+		Cobol85GrammarContextFactory.configureDefaultApplicationContext();
+
+		final File inputFile = new File("src/test/resources/org/cobol85/nist/OBNC1M.CBL");
+		final Cobol85ParseTestRunner runner = new Cobol85ParseTestRunnerImpl();
+		runner.parseFile(inputFile, null);
+	}
+}
