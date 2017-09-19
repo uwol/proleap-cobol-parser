728299ed4e76ef332bac1dbd7e5b37848cbc27be
uwol <ulrich@wolffgang.de>
2017-09-19 12:26:52 +0200

    added test

---
+++ src/test/java/io/proleap/cobol/ast/fixed/ProgramIdNoDotTest.java
@@ -0,0 +1,18 @@
+package io.proleap.cobol.ast.fixed;
+
+import java.io.File;
+
+import io.proleap.cobol.preprocessor.CobolPreprocessor.CobolSourceFormatEnum;
+import io.proleap.cobol.runner.CobolParseTestRunner;
+import io.proleap.cobol.runner.impl.CobolParseTestRunnerImpl;
+import org.junit.Test;
+
+public class ProgramIdNoDotTest {
+
+	@Test
+	public void test() throws Exception {
+		final File inputFile = new File("src/test/resources/io/proleap/cobol/ast/fixed/ProgramIdNoDot.cbl");
+		final CobolParseTestRunner runner = new CobolParseTestRunnerImpl();
+		runner.parseFile(inputFile, CobolSourceFormatEnum.FIXED);
+	}
+}
---
+++ src/test/resources/io/proleap/cobol/ast/fixed/ProgramIdNoDot.cbl
@@ -0,0 +1,4 @@
+000010 IDENTIFICATION DIVISION.                          
+000050 PROGRAM-ID.       ABC01 
+000060******************************************************************00000000
+      * not DOT after the name of the program-id
