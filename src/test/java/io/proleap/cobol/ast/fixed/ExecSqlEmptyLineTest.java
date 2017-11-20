c9ab54c0233c1fb06ea934659b1b2471fb30221d
uwol <ulrich@wolffgang.de>
2017-11-20 18:54:53 +0100

    EXEC SQL ast unit test

---
+++ src/test/java/io/proleap/cobol/ast/fixed/ExecSqlEmptyLineTest.java
@@ -0,0 +1,18 @@
+package io.proleap.cobol.ast.fixed;
+
+import java.io.File;
+
+import io.proleap.cobol.preprocessor.CobolPreprocessor.CobolSourceFormatEnum;
+import io.proleap.cobol.runner.CobolParseTestRunner;
+import io.proleap.cobol.runner.impl.CobolParseTestRunnerImpl;
+import org.junit.Test;
+
+public class ExecSqlEmptyLineTest {
+
+	@Test
+	public void test() throws Exception {
+		final File inputFile = new File("src/test/resources/io/proleap/cobol/ast/fixed/ExecSqlEmptyLine.cbl");
+		final CobolParseTestRunner runner = new CobolParseTestRunnerImpl();
+		runner.parseFile(inputFile, CobolSourceFormatEnum.FIXED);
+	}
+}
