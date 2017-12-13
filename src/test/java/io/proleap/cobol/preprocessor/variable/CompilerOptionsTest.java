f510b3bc30e7001695179708a6e6660c73d367b2
uwol <ulrich@wolffgang.de>
2017-12-13 12:55:50 +0100

    additions to CBL statement not recognized #40

---
+++ src/test/java/io/proleap/cobol/preprocessor/variable/CompilerOptionsTest.java
@@ -0,0 +1,26 @@
+package io.proleap.cobol.preprocessor.variable;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import io.proleap.cobol.preprocessor.CobolPreprocessor.CobolSourceFormatEnum;
+import io.proleap.cobol.preprocessor.impl.CobolPreprocessorImpl;
+
+public class CompilerOptionsTest {
+
+	@Test
+	public void test() throws Exception {
+		final File inputFile = new File(
+				"src/test/resources/io/proleap/cobol/preprocessor/variable/CompilerOptions.cbl");
+		final String preProcessedInput = new CobolPreprocessorImpl().process(inputFile, CobolSourceFormatEnum.TANDEM);
+
+		final File expectedFile = new File(
+				"src/test/resources/io/proleap/cobol/preprocessor/variable/CompilerOptions.cbl.preprocessed");
+		final String expected = FileUtils.readFileToString(expectedFile);
+		assertEquals(expected, preProcessedInput);
+	}
+}
