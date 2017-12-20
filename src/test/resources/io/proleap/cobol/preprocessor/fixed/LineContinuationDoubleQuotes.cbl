2cebae8e65a1687c41efdbaeb313f7d974bcd076
uwol <ulrich@wolffgang.de>
2017-12-21 00:21:08 +0100

    added ignored TODO tests

---
+++ src/test/resources/io/proleap/cobol/preprocessor/fixed/LineContinuationDoubleQuotes.cbl
@@ -0,0 +1,5 @@
+000000 IDENTIFICATION DIVISION.                                         12345678
+000000 PROGRAM-ID. LNECONTDBLQ.                                         12345678
+000000 PROCEDURE DIVISION.                                              12345678
+000000    DISPLAY '----'                                                12345678
+000000-      '===='                                                     12345678
---
+++ src/test/resources/io/proleap/cobol/preprocessor/fixed/LineContinuationWhitespace.cbl
@@ -0,0 +1,5 @@
+000100 IDENTIFICATION DIVISION.                                         12345678
+000100 PROGRAM-ID. LINECONTWS.                                          12345678
+000100 PROCEDURE DIVISION                                               12345678
+000100    DISPLAY  "this is some literal with trailing whitespace       12345678
+000100-        " and it ends here".                                     12345678
