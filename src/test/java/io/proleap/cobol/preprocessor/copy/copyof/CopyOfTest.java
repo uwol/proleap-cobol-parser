c987f8077e91aca8e4ef288c962992593f4c05a4
Jörg Preiß <joerg.preiss@slesa.de>
2017-12-21 23:37:19 +0100

    Adding test for copying files with 'of' statement

---
+++ src/test/java/io/proleap/cobol/preprocessor/copy/copyof/CopyOfTest.java
@@ -0,0 +1,55 @@
+package io.proleap.cobol.preprocessor.copy.copyof;
+
+import com.google.common.collect.Lists;
+import io.proleap.cobol.asg.params.CobolParserParams;
+import io.proleap.cobol.asg.params.impl.CobolParserParamsImpl;
+import io.proleap.cobol.preprocessor.CobolPreprocessor.CobolSourceFormatEnum;
+import io.proleap.cobol.preprocessor.impl.CobolPreprocessorImpl;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class CopyOfTest {
+
+	private static final String DIR = "src/test/resources/io/proleap/cobol/preprocessor/copy/copyof";
+
+	@Test
+	public void testCopyBookDirectories() throws Exception {
+		final File copyBookDirectory = new File(DIR + "/copybooks");
+		final ArrayList<File> copyBookDirectories = Lists.newArrayList(copyBookDirectory);
+
+		final CobolParserParams params = new CobolParserParamsImpl();
+		params.setCopyBookDirectories(copyBookDirectories);
+
+		final File inputFile = new File(DIR + "/CopyOf.cbl");
+		final String preProcessedInput = new CobolPreprocessorImpl().process(inputFile, CobolSourceFormatEnum.VARIABLE,
+				params);
+
+		final File expectedFile = new File(DIR + "/CopyOf.cbl.preprocessed");
+		final String expected = FileUtils.readFileToString(expectedFile);
+		assertEquals(expected, preProcessedInput);
+	}
+/*
+	@Test
+	public void testCopyBookFiles() throws Exception {
+		final File copyBookFile1 = new File(DIR + "/copybooks/CopyReplace1.cpy");
+		final File copyBookFile2 = new File(DIR + "/copybooks/CopyReplace2.cpy");
+		final ArrayList<File> copyBookFiles = Lists.newArrayList(copyBookFile1, copyBookFile2);
+
+		final CobolParserParams params = new CobolParserParamsImpl();
+		params.setCopyBookFiles(copyBookFiles);
+
+		final File inputFile = new File(DIR + "/CopyReplace.cbl");
+		final String preProcessedInput = new CobolPreprocessorImpl().process(inputFile, CobolSourceFormatEnum.VARIABLE,
+				params);
+
+		final File expectedFile = new File(DIR + "/CopyReplace.cbl.preprocessed");
+		final String expected = FileUtils.readFileToString(expectedFile);
+		assertEquals(expected, preProcessedInput);
+	}
+	*/
+}
