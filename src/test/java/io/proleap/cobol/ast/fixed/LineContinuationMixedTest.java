047512dee785314430a52ad36aac977d97a91b19
uwol <ulrich@wolffgang.de>
2017-12-21 17:05:04 +0100

    added unit test with some line continuations #54

---
+++ src/test/java/io/proleap/cobol/ast/fixed/LineContinuationMixedTest.java
@@ -0,0 +1,18 @@
+package io.proleap.cobol.ast.fixed;
+
+import java.io.File;
+
+import io.proleap.cobol.preprocessor.CobolPreprocessor.CobolSourceFormatEnum;
+import io.proleap.cobol.runner.CobolParseTestRunner;
+import io.proleap.cobol.runner.impl.CobolParseTestRunnerImpl;
+import org.junit.Test;
+
+public class LineContinuationMixedTest {
+
+	@Test
+	public void test() throws Exception {
+		final File inputFile = new File("src/test/resources/io/proleap/cobol/ast/fixed/LineContinuationMixed.cbl");
+		final CobolParseTestRunner runner = new CobolParseTestRunnerImpl();
+		runner.parseFile(inputFile, CobolSourceFormatEnum.FIXED);
+	}
+}
